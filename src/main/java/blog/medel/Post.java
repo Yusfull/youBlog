f63c142b036d7e2beb7e91e529222503a7772e47
Khunga Eusuph Qolo <profqolo@gmail.com>
2018-01-17 14:45:38 +0200

    Formatting and spacing fix.

--- src/main/java/blog/medel/Post.java
+++ src/main/java/blog/medel/Post.java
@@ -16,18 +16,13 @@ public class Post {
 	private String body;
 	private User author;
 	private Date date = new Date();
-	
-	
-	
-	
+
 	/**
 	 * 
 	 */
 	public Post() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 
+	}
 
 	/**
 	 * @param id
@@ -44,78 +39,89 @@ public class Post {
 		this.author = author;
 	}
 
-
 	/**
 	 * @return the id
 	 */
 	public Long getId() {
 		return id;
 	}
+
 	/**
-	 * @param id the id to set
+	 * @param id
+	 *            the id to set
 	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	/**
 	 * @return the title
 	 */
 	public String getTitle() {
 		return title;
 	}
+
 	/**
-	 * @param title the title to set
+	 * @param title
+	 *            the title to set
 	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	/**
 	 * @return the body
 	 */
 	public String getBody() {
 		return body;
 	}
+
 	/**
-	 * @param body the body to set
+	 * @param body
+	 *            the body to set
 	 */
 	public void setBody(String body) {
 		this.body = body;
 	}
+
 	/**
 	 * @return the author
 	 */
 	public User getAuthor() {
 		return author;
 	}
+
 	/**
-	 * @param author the author to set
+	 * @param author
+	 *            the author to set
 	 */
 	public void setAuthor(User author) {
 		this.author = author;
 	}
+
 	/**
 	 * @return the date
 	 */
 	public Date getDate() {
 		return date;
 	}
+
 	/**
-	 * @param date the date to set
+	 * @param date
+	 *            the date to set
 	 */
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
 		return "Post [id=" + id + ", title=" + title + ", body=" + body + ", author=" + author + ", date=" + date + "]";
 	}
-	
-	
-	
-	
+
 }
